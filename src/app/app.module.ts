b9a9317c37251ddd37065f4cb29fa5f5fce0d727
owlruslan <ruslanusm6@gmail.com>
2018-01-25 18:38:14 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Imports of MDC Angular components
+import {
+  MdcButtonModule,
+  MdcFabModule,
+} from '@angular-mdc/web';
+import { MdcToolbarModule, MdcIconModule } from '@angular-mdc/web';
+import { MdcElevationModule, MdcDrawerModule, MdcListModule, MdcCardModule } from '@angular-mdc/web';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdcButtonModule,
+    MdcFabModule,
+    MdcToolbarModule,
+    MdcIconModule,
+    MdcElevationModule,
+    MdcDrawerModule,
+    MdcListModule,
+    MdcCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
