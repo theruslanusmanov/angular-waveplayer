c5925e482dde8e0b2b5bbb2d919b83d9aa3d617f
owlruslan <ruslanusm6@gmail.com>
2018-01-28 23:17:40 +0500

    basic player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 // Imports of MDC Angular components
 import {
   MdcButtonModule,
@@ -10,11 +11,24 @@ import { MdcToolbarModule, MdcIconModule } from '@angular-mdc/web';
 import { MdcElevationModule, MdcDrawerModule, MdcListModule, MdcCardModule } from '@angular-mdc/web';
 
 import { AppComponent } from './app.component';
+import { WaveSurferComponent } from './components/wavesurfer/wavesurfer.component';
+/*
+requestAnimationFrame(() => {
 
+  const wavesurfer = WaveSurfer.create({
+    container: '.player-container',
+    waveColor: '#a6c8fc',
+    progressColor: '#fff'
+  });
+  wavesurfer.load('./../assets/audio/test.mp3');
+  wavesurfer.playPause();
+});
+*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WaveSurferComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/wavesurfer/wavesurfer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as WaveSurfer from 'wavesurfer.js';
+
+@Component({
+    selector: 'wave-surfer',
+    templateUrl: 'wavesurfer.component.html',
+    styleUrls: ['wavesurfer.component.sass']
+})
+export class WaveSurferComponent implements OnInit {
+
+    ngOnInit() {
+        const wavesurfer = WaveSurfer.create({
+            container: '.player-container',
+            waveColor: '#a6c8fc',
+            progressColor: '#fff'
+        });
+        wavesurfer.load('./../assets/audio/test.mp3');
+        /*wavesurfer.on('ready', function () {
+            wavesurfer.play();
+          });*/
+    }
+}
