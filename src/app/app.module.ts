f4cac548a0883d1784d45b9ba81dd529c0cd3330
owlruslan <ruslanusm6@gmail.com>
2018-03-10 00:18:17 +0300

    add router in main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 // Imports of MDC Angular components
 import {
@@ -19,14 +20,36 @@ import { AppComponent } from './app.component';
 import { WaveSurferComponent } from './components/wavesurfer/wavesurfer.component';
 import { PlaylistComponent } from './components/playlist/playlist.component';
 import { PlaylistService } from './services/playlist.service';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'home',
+    component: AppComponent,
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     WaveSurferComponent,
-    PlaylistComponent
+    PlaylistComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     MdcButtonModule,
     MdcFabModule,
